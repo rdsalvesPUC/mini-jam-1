63b035566fc6e22fdfc3539e6c5f2bdbf5fa8b64
rodrigo.salves <s.rodrigo5@pucpr.edu.br>
2025-03-27 22:16:17 -0300

    queda das gotas ainda nao funciona

--- sketch.js
+++ sketch.js
@@ -1,57 +1,69 @@
 const _SPEED = 0;
 const _QUANTITY = 0;
-const _SPAWN = 0;
+const _SPAWN = 60;
 
 let bkgColor = '#7BDDF6';
 let drop;
 let bacia;
+let dropImages = [];
 
-function preload(){
-  drop_xl = loadImage('/img-assets/drop-xl.png');
-  drop_l = loadImage('/img-assets/drop-l.png');
-  drop_s = loadImage('/img-assets/drop-s.png');
-  // bacia = loadImage('');
+function preload() {
+  dropImages = [
+    loadImage('/img-assets/drop-s.png'),
+    loadImage('/img-assets/drop-l.png'),
+    loadImage('/img-assets/drop-xl.png')
+  ];
+
+  // bacia = loadImage('/img-assets/bacia.png');
 }
 
 function setup() {
   createCanvas(500, 800);
+  frameRate(1);
 }
 
 function draw() {
   background(bkgColor);
-  drawDrop();
+  // drawDrop();
+  // updateDrop();
+  drop = generateDrop();
+
+  while (drop.y < height) {
+    image(drop.img, drop.x, drop.y);
+    drop.y += 1 * drop.speed;
+  }
+  
 }
 
-
-
-//todos os desenhos
-  //ceu
-  //gotas
-  //bacia
-  //background
-
-///dinamicas do jogo
-//contador de pontos
-//gotas
-  //geração randomica das gotas
-  //velocidade das gotas
-  //queda das gotas
-//bacia
-  //movimento lateral
-  //area de contato da bacia com as gotas
+function updateDrop() {
+  if (frameCount % _SPAWN === 0) {
+    drawDrop();
+  }
+}
 
 function drawDrop() {
-  image(drop_xl, 100, 100);
+  drop = generateDrop();
+  image(drop.img, drop.x, drop.y);
+  
+  while (drop.y < height) {
+    drop.y += 1 * drop.speed;
+    image(drop.img, drop.x, drop.y);
+  }
+  // if (drop.y > (height + drop.img.height)) {
+  //   drop.y = 0;
+  // }
 }
 
 function generateDrop(){
-  
-}
+  let dropSort = random(dropImages);
+  let xSort = constrain(random(width), dropSort.width/2, width - dropSort.width/2);
+  let ySort = 0;
+  let speedSort = random(1, 5);
 
-function acellDrop(){
-  
-}
-
-function fallDrop(){
-  
+  return {
+    img: dropSort,
+    x: xSort,
+    y: ySort,
+    speed: speedSort
+  };
 }
